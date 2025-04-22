64576a655ca002d69864a51f202c191553a761d0
jeacd <joao.euller@academico.ifpb.edu.br>
2025-04-22 19:42:23 -0300

    add methods post and put

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import json
 
 app = Flask(__name__)
@@ -12,6 +12,13 @@ def escreverJson(dados):
     with open('dados.json', mode='w', encoding='utf-8') as json_file:
         json.dump(dados, json_file, ensure_ascii=False, indent=4)
 
+def validarInstituicao(content):
+    if content.get('CO_UF') is None:
+        return False
+    elif int(content['CO_UF']) < 11 or int(content['CO_UF']) > 55:
+        return False
+    else:
+        return True
 
 @app.get('/instituicoesensino')
 def getInstituicoesEnsino():
@@ -26,19 +33,52 @@ def getInstituicaoEnsino(cod_entidade):
     if len(instituicoesFiltradas) > 0:
         return jsonify(instituicoesFiltradas), 200
     else:
-        return jsonify({'Erro': 'ID não encontrado'}), 404
+        return jsonify({'erro': 'Instituição não encontrada'}), 404
     
 @app.delete('/instituicoesensino/<cod_entidade>')
 def deleteInstituicaoEnsino(cod_entidade):
     instituicoes = lerJson()
-    instituicoesAposDelete = [instituicao for instituicao in instituicoes if instituicao['CO_ENTIDADE'] != cod_entidade]
+    instituicoesFiltradas = [instituicao for instituicao in instituicoes if instituicao['CO_ENTIDADE'] != cod_entidade]
     
-    if len(instituicoesAposDelete) < len(instituicoes):
-        escreverJson(instituicoesAposDelete)
-        return jsonify({'mensagem': 'Delete realizado com sucesso'}), 200
+    if len(instituicoesFiltradas) < len(instituicoes):
+        escreverJson(instituicoesFiltradas)
+        return jsonify({'mensagem': 'Instituição removida com sucesso'}), 200
     else:
-        return jsonify({'Erro': 'Instituição não encontrada'}), 404
+        return jsonify({'erro': 'Instituição não encontrada'}), 404
 
+@app.post('/instituicoesensino')
+def postInstituicaoEnsino():
+    instituicoes = lerJson()
+    content = request.get_json()
+    isValid = validarInstituicao(content)
+    
+    if (isValid):
+        instituicoes.append(content)
+        escreverJson(instituicoes)
+        return jsonify({"mensagem": "Cadastro realizado", "dados": content}), 200
+    
+    return jsonify({'erro': 'Instituição não cadastrada'}), 406
+
+@app.put('/instituicoesensino')
+def updateInstituicaoEnsino():
+    instituicoes = lerJson()
+    content = request.get_json()
+    countIndex = -1
+    instituicaoEncontrada = False
+    
+    for instituicao in instituicoes:
+        countIndex = countIndex + 1
+        if instituicao['CO_ENTIDADE'] == content['CO_ENTIDADE']:
+            instituicaoEncontrada = True
+            break
+        
+    if instituicaoEncontrada:
+        instituicoes[countIndex].update(content)
+        escreverJson(instituicoes)
+        return jsonify({'message': 'Instituição atualizada com sucesso!'}), 200
+    else:
+        return jsonify({'erro': 'Instituição não encontrada.'}), 404
+            
 
 if __name__ == '__main__':
     app.run()
